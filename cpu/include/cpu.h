d88730c0d526d6d262975fef47516010553ea651
GianfrancoRiccelli <griccelli@frba.utn.edu.ar>
2022-05-03 23:54:00 -0300

    arranco la cpu

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -1,1 +1,9 @@
+#ifndef CPU_H_
+#define CPU_H_
+#include <commons/config.h>
+#include "estructurascpu.h"
+#include <commons/log.h>
 
+
+
+#endif
--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -1,1 +1,17 @@
+#include "../include/cpu.h"
 
+int main(void){
+
+    t_config *iconfig;
+    iconfig = config_create("config/cpu.config");
+
+    t_config_cpu *cpuconfig;
+    cpuconfig -> entradas_tlb = config_get_int_value(iconfig, "ENTRADAS_TLB");
+    cpuconfig -> reemplazo_tlb = config_get_string_value(iconfig, "REEMPLAZO_TLB");
+    cpuconfig -> retardo_noop = config_get_int_value(iconfig, "RETARDO_NOOP");
+    cpuconfig -> ip_memoria = config_get_string_value(iconfig, "IP_MEMORIA");
+    cpuconfig -> puerto_memoria = config_get_string_value(iconfig, "PUERTO_MEMORIA");
+    cpuconfig -> puerto_escucha_dispatch = config_get_string_value(iconfig, "PUERTO_ESCUCHA_DISPATCH");
+    cpuconfig -> puerto_escucha_interrupt = config_get_string_value(iconfig, "PUERTO_ESCUCHA_INTERRUPT");
+    
+};
